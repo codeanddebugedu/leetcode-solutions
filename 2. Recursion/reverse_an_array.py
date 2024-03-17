6abdb4b4686a303bdbb4513b22728db7c4d9f94d
himagm <162509256+himagm@users.noreply.github.com>
2024-03-17 11:02:36 -0700

    alternate method to reverse an array using recursion (#14)

--- 2. Recursion/reverse_an_array.py
+++ 2. Recursion/reverse_an_array.py
@@ -4,8 +4,21 @@ def reverse_list(lst, start, end):
     lst[start], lst[end] = lst[end], lst[start]
     reverse_list(lst, start + 1, end - 1)
 
+#Alternate Method using just one Index
+def revAr(arr, ind):
+    n = len(arr)
+    if ind == n //2:
+        print(arr)
+        return
+    arr[ind], arr[n-1-ind] = arr[n-1-ind], arr[ind]
+    revAr(arr, ind+1)
+
+
 
 if __name__ == "__main__":
     my_list = [3, 5, 1, 7, 8, 6, 5, 100]
     reverse_list(my_list, 0, len(my_list) - 1)
     print(my_list)
+
+    new_list = [3, 5, 1, 7, 8, 6, 5, 100]
+    revAr(new_list, 0)
