2960c593b7af6c437eccb34bc2e4cb373992c3aa
unknown <siddeshdagwal@gmail.com>
2024-03-18 11:34:14 +0530

    added bubble sort code using flag

--- 4. Sorting/bubble-sort.py
+++ 4. Sorting/bubble-sort.py
@@ -22,3 +22,22 @@ def bubble_sort(arr):
 arr = [64, 25, 12, 22, 11]
 bubble_sort(arr)
 print(arr)
+
+
+# Bubble Sort Implementation with Optimization using flag
+def bubble_sort1(arr):
+    n = len(arr)
+    for i in range(n - 1, 0, -1):
+        swap = False
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                swap = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        if not swap:
+            # exits as there were no swaps meaning list is sorted
+            return
+
+
+arr = [64, 25, 12, 22, 11, 27, 0, -15]
+bubble_sort1(arr)
+print(arr)
