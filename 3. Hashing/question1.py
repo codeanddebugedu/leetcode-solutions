4073a3abc69f6c90f1e2a4c5d5e41debf964466c
darekillr <anant2011puri@gmail.com>
2024-03-18 18:56:19 +0530

    Q1 Hashing Solution

--- 3. Hashing/question1.py
+++ 3. Hashing/question1.py
@@ -3,3 +3,16 @@ Question link
 
 https://www.codingninjas.com/studio/problems/count-frequency-in-a-range_8365446
 """
+
+from typing import *
+
+
+def countFrequency(n: int, m: int, edges: List[List[int]]):
+
+    mylst = [0] * n
+
+    for x in edges:
+        if x > n:
+            continue
+        mylst[x - 1] += 1
+    return mylst
