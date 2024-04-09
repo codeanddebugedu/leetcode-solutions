284d021ee9c68ee0b189a4786afdadc76fbece5f
Code and Debug <codeanddebugedu@gmail.com>
2024-04-09 07:52:38 +0530

    merge-sort-refactor

--- 4. Sorting/merge-sort.py
+++ 4. Sorting/merge-sort.py
@@ -5,23 +5,6 @@ Space Complexity: O(n)
 """
 
 
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    # Divide the array into two halves
-    mid = len(arr) // 2
-    left_half = arr[:mid]
-    right_half = arr[mid:]
-
-    # Recursively sort each half
-    left_half = merge_sort(left_half)
-    right_half = merge_sort(right_half)
-
-    # Merge the sorted halves
-    return merge(left_half, right_half)
-
-
 def merge(left, right):
     merged = []
     i, j = 0, 0
@@ -45,6 +28,23 @@ def merge(left, right):
         j += 1
 
     return merged
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    # Divide the array into two halves
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    # Recursively sort each half
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    # Merge the sorted halves
+    return merge(left_half, right_half)
 
 
 arr = [64, 34, 25, 12, 22, 11, 90]
