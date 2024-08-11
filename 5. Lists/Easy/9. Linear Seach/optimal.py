7591a9da80296a67143536b382c5d71ca3803d29
Rajuchaurasiya <89697045+Rajuchaurasiya@users.noreply.github.com>
2024-08-11 23:58:26 +0530

    added command to linear search

--- 5. Lists/Easy/9. Linear Seach/optimal.py
+++ 5. Lists/Easy/9. Linear Seach/optimal.py
@@ -1,6 +1,6 @@
 #This is a linear search code with time complexity o(n) 
 # :')
-
+#this is new.git stsuts
 def linearSearch(n: int, num: int, arr: [int]) -> int:
     for i in range(0, len(arr)):
         if arr[i] == num:
