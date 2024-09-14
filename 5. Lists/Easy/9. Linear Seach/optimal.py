b2cab009f05fc2e4bdcad85ca9c6211209725637
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2024-09-14 19:48:24 -0300

    added comments

--- 5. Lists/Easy/9. Linear Seach/optimal.py
+++ 5. Lists/Easy/9. Linear Seach/optimal.py
@@ -1,6 +1,9 @@
 #This is a linear search code with time complexity o(n) 
 # :')
 
+#  doing this as of the part of the open src contribution 
+
+
 def linearSearch(n: int, num: int, arr: [int]) -> int:
     for i in range(0, len(arr)):
         if arr[i] == num:
