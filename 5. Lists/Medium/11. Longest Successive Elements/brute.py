53503c1ee810f508736001969fcc1c19dbd18142
Code and Debug <codeanddebugedu@gmail.com>
2024-12-04 18:03:32 +0530

    longest successive elements solution refactor bruteforce

--- 5. Lists/Medium/11. Longest Successive Elements/brute.py
+++ 5. Lists/Medium/11. Longest Successive Elements/brute.py
@@ -8,20 +8,14 @@ Space complexity -> O(1)
 """
 
 
-def linearSearch(arr, num):
-    for i in arr:
-        if i == num:
-            return True
-    return False
-
-
 def longestSuccessiveElements(arr: List[int]) -> int:
     max_count = 0
-    for i in arr:
-        x = i
+    n = len(arr)
+    for i in range(0, n):
+        num = arr[i]
         count = 1
-        while linearSearch(arr, x + 1):
-            x += 1
+        while num + 1 in arr:
             count += 1
+            num += 1
         max_count = max(max_count, count)
     return max_count
