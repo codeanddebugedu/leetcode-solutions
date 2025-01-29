8e86930dad0623470481fe6ba04a0117ed3d9594
Code and Debug <codeanddebugedu@gmail.com>
2025-01-29 17:00:06 +0530

    majoirty element solution refactor

--- 5. Lists/Medium/5. Leetcode 169 - Majority Element/optimal.py
+++ 5. Lists/Medium/5. Leetcode 169 - Majority Element/optimal.py
@@ -12,14 +12,14 @@ Space complexity -> O(1)
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        candidate = None
+        candidate = nums[0]
         count = 0
         for i in range(0, len(nums)):
-            if nums[i] == candidate:
-                count += 1
-            elif count == 0:
+            if count == 0:
                 candidate = nums[i]
                 count = 1
+            elif nums[i] == candidate:
+                count += 1
             else:
                 count -= 1
         return candidate
