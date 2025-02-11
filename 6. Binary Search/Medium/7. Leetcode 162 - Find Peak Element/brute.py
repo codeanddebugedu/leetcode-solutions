0fee870f7089a8a2ac01ebbb8baaa25b1e857650
Code and Debug <codeanddebugedu@gmail.com>
2025-02-11 15:54:19 +0530

    find peak element brute force solution refactor

--- 6. Binary Search/Medium/7. Leetcode 162 - Find Peak Element/brute.py
+++ 6. Binary Search/Medium/7. Leetcode 162 - Find Peak Element/brute.py
@@ -9,8 +9,14 @@ Space complexity -> O(1)
 
 
 class Solution:
-    def findPeakElement(self, arr: List[int]) -> int:
-        n = len(arr)
-        for i in range(n):
-            if (i == 0 or arr[i - 1] < arr[i]) and (i == n - 1 or arr[i] > arr[i + 1]):
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+        elif nums[0] > nums[1]:
+            return 0
+        elif nums[n - 1] > nums[n - 2]:
+            return n - 1
+        for i in range(1, n - 1):
+            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
                 return i
