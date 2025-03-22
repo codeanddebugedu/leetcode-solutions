224dbcfa7c4226b9ddf866da2c8314bac1ec2887
Code and Debug <codeanddebugedu@gmail.com>
2025-03-22 16:17:04 +0530

    combination sum 3 solution refactor

--- 13. Advance Recursion/Medium/9. Combination Sum 3/optimal.py
+++ 13. Advance Recursion/Medium/9. Combination Sum 3/optimal.py
@@ -1,21 +1,18 @@
 class Solution:
-    def func(self, sum, last, nums, k, ans):
-        if sum == 0 and len(nums) == k:
+    def func(self, n, Sum, last, nums, k, ans):
+        if Sum == n and len(nums) == k:
             ans.append(list(nums))
             return
-        if sum <= 0 or len(nums) > k:
+        if Sum > n or len(nums) > k:
             return
 
         for i in range(last, 10):
-            if i <= sum:
-                nums.append(i)
-                self.func(sum - i, i + 1, nums, k, ans)
-                nums.pop()
-            else:
-                break
+            nums.append(i)
+            self.func(n, Sum + i, i + 1, nums, k, ans)
+            nums.pop()
 
     def combinationSum3(self, k, n):
         ans = []
         nums = []
-        self.func(n, 1, nums, k, ans)
+        self.func(n, 0, 1, nums, k, ans)
         return ans
